dd5b3a5d412188bc96221e73a20dac42303821aa
Lee SangHun <mysh1105@gmail.com>
2023-06-08 22:08:49 +0900

    need refactoring

--- subpage.js
+++ subpage.js
@@ -1,10 +1,12 @@
+export let movieId = searchParam("id");
+
 function searchParam(key) {
   return new URLSearchParams(location.search).get(key);
 }
 document.addEventListener("DOMContentLoaded", () => {
   fetchMovie();
 });
-let movieId = 250;
+
 const options = {
   method: "GET",
   headers: {
@@ -13,12 +15,12 @@ const options = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOGEyN2RhZGEzZmIyMzZjOWFiMDEwY2IyZmRhNmQ4MyIsInN1YiI6IjY0NzU2MTY1ZGQyNTg5MDBjMzI0ZGZkNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HDK-jrZxT07rId8Dq5fbX5H9FTAJp6IB0RmWisVR8QI",
   },
 };
-const section1 = document.querySelector("#card");
+const section = document.querySelector("#card");
 const fetchMovie = async () => {
   const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}`, options)
     .then((res) => res.json())
     .then((data) => {
-      section1.innerHTML = `<li class="posterCard" >
+      section.innerHTML = `<li class="posterCard" >
                               <img id="movieImage" src="https://image.tmdb.org/t/p/w500${data.poster_path}" id=${data.id}>
                               <div id="cadeMargin">
                               <h2 id="movieTitle">${data.title}</h2>
